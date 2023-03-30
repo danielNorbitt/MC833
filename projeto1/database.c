fafb196993a2792d25018d8fc965c353974e0bc2
daniel.oliveira <daniel.oliveira@nubank.com.br>
2023-03-29 22:11:28 -0300

    first

---
+++ projeto1/database.c
@@ -0,0 +1,9 @@
+
+void addProfile();
+void findProfileBySkill();
+void findProfileByCourse();
+void findProfileByGraduationYear(int graduationYear);
+void findByEmail();
+void removeProfileByEmail();
+void downloadProfileByEmial();
+
---
+++ projeto1/response.c
@@ -0,0 +1,5 @@
+typedef struct {
+    String name;
+    String email;
+} Response
+
